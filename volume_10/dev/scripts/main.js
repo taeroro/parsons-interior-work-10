d834b30364b6b47f51380d694305ea9f6ac3d949
Ryan Fan <ryanfan1996@gmail.com>
2020-01-09 00:27:47 -0500

    faculty done, not responsive

--- volume_10/dev/scripts/main.js
+++ volume_10/dev/scripts/main.js
@@ -313,15 +313,15 @@ window.onload = function () {
         },false);
     }
 
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+    document.querySelectorAll('a[href^="#"]').forEach(function(anchor){
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
-});
 };
 
 
